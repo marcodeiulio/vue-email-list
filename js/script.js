3829e590df24b19b58890bafc108de6e60dcd87f
marcodeiulio <mdeiulio@icloud.com>
2022-02-04 16:47:42 +0100

    - Added getRandomEmails(num)

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ Vue.config.devtools = true
 const root = new Vue({
 	el: '#root',
 	data: {
-		message: 'Hello Vue!'
+		emails: [],
+	},
+	methods: {
+		getRandomEmails(num) {
+			for (let i = 0; i < num; i++) {
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+					this.emails.push(res.data.response);
+					console.log(this.emails);
+				});
+			}
+		}
+	},
+	mounted() {
+		this.getRandomEmails(10)
 	}
-})
-
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-	console.log(res.data);
-})
+});
